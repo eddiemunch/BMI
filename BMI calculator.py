cca1a7f8250f16c81b1bed00b065e32d97fd8fb3
eddiemunch <69631966+eddiemunch@users.noreply.github.com>
2022-02-20 11:06:15 +0000

    Add files via upload

--- BMI calculator.py
+++ BMI calculator.py
@@ -18,19 +18,23 @@ def bmicalc():
 def measurements():
     global measurementW
     global measurementH
-    measurementW = input("what weight measurement would you like to use? \nkilograms  \nnewtons \ngrams \n")
+    measurementW = input("what weight measurement would you like to use? \nkilograms  \nnewtons \ngrams \npounds \nstone \n")
     measurementW = measurementW.lower()
-    measurementH = input("What height measurement would you like to use? \nmeters \ncentimeters \n")
+    measurementH = input("What height measurement would you like to use? \nmeters \ncentimeters \nfeet \ninches \n")
     measurementH = measurementH.lower()
     inputs()
 
 def inputs():
+    global feet
+    global inches
+    global kg1
+    global kg2
     global weight
     global height
     if measurementW == "kilograms":
-        weight = float(input("Please Input your weight: "))
+        weight = float(input("Please enter your weight: "))
     if measurementW == "newtons":
-        weight = float(input("Please Input your weight: "))
+        weight = float(input("Please enter your weight: "))
         weight = weight/9.81
     if measurementH == "meters":
         height = float(input("Please enter your height: "))
@@ -40,12 +44,28 @@ def inputs():
     if measurementW == "grams":
         weight = float(input("Please enter your weight: "))
         weight = weight/1000
+    if measurementW == "pounds":
+        weight = float(input("Please enter your weight: "))
+        weight = weight/2.205
+    if measurementW == "stone":
+        weight = float(input("Please enter your weight: "))
+        weight = weight*6.35
+    if measurementH == "feet":
+        print("for this we will split your height into inches and feet so first: ")
+        feet = float(input("Please enter your height (feet excluding inches): "))
+        inches = float(input("Please enter your height (the remainder in inches): "))
+        kg1 = feet/3.281
+        kg2 = inches/39.37
+        height = kg1+kg2
+    if measurementH == "inches":
+        height = float(input("Please enter your height: "))
+        height = height/39.37
     bmicalc()
     choice()
 
 def choice():
     global close
-    close = input("would you like to close the program? \n(Y/N) ")
+    close = input("would you like to close the program? \n(Y/N) \n")
     if close == "Y":
         print("closing program")
         time.sleep(1)
